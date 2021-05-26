cfcb86661140e6be39d5422483ea5b59f5d14730
AShtyr <Shtirlyaeva.aa@mail.ru>
2021-05-26 22:46:15 +0300

    brackets

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -1,3 +1,7 @@
+
+
+l_ = []
+
 def check_brackets(brackets_row: str) -> bool:
     """
     Check whether input string is a valid bracket sequence
@@ -5,5 +9,14 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
+    l_ = brackets_row.split(' ')
+    l_1 = [i for i in l_ if i != '()']
+    for i in range(len(l_1)):
+        if l_1[i] != l_[-(i + 1)]:
+            return True
+        else:
+            return False
 
-    return False
+
+
+
