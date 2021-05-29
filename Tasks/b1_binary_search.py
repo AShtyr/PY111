1295de0844ad6b8799560ecdbac5ce2f12b808af
AShtyr <Shtirlyaeva_aa@mail.ru>
2021-05-29 12:21:00 +0300

    binary_search

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -10,4 +10,26 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :return: Index of element if it's presented in the arr, None otherwise
     """
 
+    a = 0
+    b = len(arr)-1
+    mid = (a+b)//2
+
+    while a <= b:
+        if arr[mid] == elem:
+            break
+        else:
+            if elem < arr[mid]:
+                b = mid - 1
+                mid = (a + b) // 2
+            else:
+                a = mid + 1
+                mid = (a + b) // 2
+    else:
+        return None
+
+
+    while mid > 0 and arr[mid-1] == elem:
+        mid -= 1
+    return mid
+
 
