3e6c8e6d9b50a7ed56e87216b77feff2a9df933f
AShtyr <Shtirlyaeva.aa@mail.ru>
2021-07-03 16:53:25 +0300

    brackets

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,14 +5,13 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
-    check_list = []
     count = 0
     for i in range(len(brackets_row.replace(' ', ''))):
         if brackets_row[i] == '(':
             count += 1
         else:
             count -= 1
-        check_list.append(count)
-
-    if all(k >= 0 for k in check_list) and count == 0:
+        if count < 0:
+            return False
+    if count == 0:
         return True
